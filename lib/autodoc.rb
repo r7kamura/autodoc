1a04c363dd4cd6bbce7d928ac84c130366afd638
Ryo Nakamura <r7kamura@gmail.com>
2013-11-30 23:33:26 +0900

    Refactor reduce part

--- lib/autodoc.rb
+++ lib/autodoc.rb
@@ -23,17 +23,15 @@ if ENV["AUTODOC"]
     end
 
     config.after(:suite) do
-      table = Hash.new {|table, key| table[key] = [] }
-      Autodoc.contexts.each do |context|
-        table[context.example.file_path] << Autodoc::Document.render(context)
-      end
-
-      table.each do |path, documents|
+      table = Autodoc.contexts.group_by {|context| context.example.file_path }
+      table.each do |path, contexts|
         pathname = Rails.root.join("doc")
         pathname += ENV["AUTODOC"] if ENV["AUTODOC"] != "1"
         pathname += path.gsub("./spec/requests/", "").gsub("_spec.rb", ".md")
         pathname.parent.mkpath
-        pathname.open("w") {|file| file << documents.join("\n").rstrip + "\n" }
+        pathname.open("w") do |file|
+          file << contexts.map {|context| Autodoc::Document.render(context) }.join("\n").rstrip + "\n"
+        end
       end
     end
   end
