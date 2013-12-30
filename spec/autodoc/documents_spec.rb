17c9eba7ef4c02ec982c98fb561f2a107b2b85e4
Ryo Nakamura <r7kamura@gmail.com>
2013-12-30 13:40:26 +0900

    Refactor documents_spec.rb
    * It would be better to use double rather than any_instance.stub
    * use `context` to group conditional examples

--- spec/autodoc/documents_spec.rb
+++ spec/autodoc/documents_spec.rb
@@ -3,29 +3,65 @@ require "spec_helper"
 describe Autodoc::Documents do
   describe "#render_toc" do
     before do
-      Autodoc::Document.any_instance.stub(:pathname){ Autodoc.configuration.pathname + pathname }
-      Autodoc::Document.any_instance.stub(:title){ title }
-
-      @documents = Autodoc::Documents.new
-      @documents.append(double(:context))
+      documents.append(context)
     end
 
-    describe "renders 'GET /recipes' in ./recipes.md" do
-      let(:title) { "GET /recipes" }
-      let(:pathname) { "./recipes.md" }
-
-      subject { @documents.send(:render_toc) }
-      it { should include("[recipes.md](recipes.md)") }
-      it { should include("[GET /recipes](recipes.md#get-recipes)") }
+    let(:documents) do
+      Autodoc::Documents.new
     end
 
-    describe "renders 'GET /admin/recipes' in ./admin/recipes.md" do
-      let(:title) { "GET /admin/recipes" }
-      let(:pathname) { "./admin/recipes.md" }
+    let(:context) do
+      mock = double(example: example, request: request)
+      mock.stub(clone: mock)
+      mock
+    end
 
-      subject { @documents.send(:render_toc) }
-      it { should include("[admin/recipes.md](admin/recipes.md)") }
-      it { should include("[GET /admin/recipes](admin/recipes.md#get-adminrecipes)") }
+    let(:example) do
+      double(file_path: file_path, full_description: full_description)
+    end
+
+    let(:file_path) do
+      "spec/requests/recipes.md"
+    end
+
+    let(:request) do
+      double(method: method)
+    end
+
+    let(:method) do
+      "GET"
+    end
+
+    context "with GET /recipes spec" do
+      let(:full_description) do
+        "Recipe #{method} /recipes returns recipes"
+      end
+
+      let(:file_path) do
+        "./spec/requests/recipes_spec.rb"
+      end
+
+      it "includes links to recipes.md" do
+        toc = documents.send(:render_toc)
+        toc.should include("[recipes.md](recipes.md)")
+        toc.should include("[GET /recipes](recipes.md#get-recipes)")
+      end
+    end
+
+    context "with GET /admin/recipes spec" do
+      let(:full_description) do
+        "Admin::Recipe #{method} /admin/recipes returns recipes for administrator"
+      end
+
+      let(:file_path) do
+        "./spec/requests/admin/recipes_spec.rb"
+      end
+
+      it "includes links to admin/recipes.md" do
+        toc = documents.send(:render_toc)
+        toc.should include("[admin/recipes.md](admin/recipes.md)")
+        toc.should include("[GET /admin/recipes](admin/recipes.md#get-adminrecipes)")
+      end
     end
   end
 end
